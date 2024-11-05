ed667ad7384c3ee663d7a5de8d14cbbbf02115ec
causeUrGnocchi <gkussunoki@gmail.com>
2024-11-05 08:22:12 -0300

    Add template for the search results page

--- handlers/ResultsHandler.go
+++ handlers/ResultsHandler.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gocolly/colly"
@@ -9,14 +9,32 @@ import (
 
 type ResultsHandler struct {}
 
-func (h ResultsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c := colly.NewCollector(
-		colly.AllowedDomains("https://jisho.org/"),
-	)
+type ResultsPageData struct {
+    Words []word
+    Search string
+}
 
-	c.OnHTML(".concept-light.clearfix", func(h *colly.HTMLElement) {
-		
+type word struct {
+    Writing string `selector:".concept_light-representation > .text"`
+    Reading string `selector:".concept_light-representation > .furigana"`
+    Meanings []string `selector:"concept_light-meanings.medium-9.columns > .meaning-meaning"`
+}
+
+func (h ResultsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    words := make([]word, 0)
+    
+    c := colly.NewCollector()
+    
+	c.OnHTML("#primary", func(container *colly.HTMLElement) {
+        container.ForEach(".concept_light.clearfix", func(i int, e *colly.HTMLElement) {
+            v := &word{}
+            e.Unmarshal(v)
+            words = append(words, *v)
+        })
 	})
 
-	fmt.Println(r.URL.Query().Get("search"))	
+    c.Visit("https://jisho.org/search/" + r.URL.Query().Get("search"))
+
+    tmpl := template.Must(template.ParseFiles("assets/html/home.html", "assets/html/results.html"))
+    tmpl.ExecuteTemplate(w, "base", ResultsPageData{Words: words, Search: r.URL.Query().Get("search")})
 }
